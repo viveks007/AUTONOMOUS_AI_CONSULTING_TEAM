d414451b4b71deff5f8b050c98069494d6f74336
Vivek Sharma <vivek.sharma0403@gmail.com>
2026-06-25 20:52:30 +0530

    Added groq configuration

---
+++ llm/groq_client.py
@@ -0,0 +1,11 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0.3,
+    groq_api_key=os.getenv("GROQ_API_KEY")
+)
